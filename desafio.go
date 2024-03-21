dce9793414e7e768424dc76041148d8c0047c421
mhoffmann76 <hoffmannmarcelo@hotmail.com>
2024-03-20 23:44:15 -0300

    desafio escalas termometricas

---
+++ desafio.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	//C =K -273
+	valorK := 373.15
+	resutadoFormula := valorK - 273.15
+
+	fmt.Println("A tempratura em graus Celsios é de:",resutadoFormula,"°C")
+
+}
